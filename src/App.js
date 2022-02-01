6f92ab12cf93f12afea45059c970abca68220c76
Adhiv Dhar <adhivd@gmail.com>
2022-01-31 16:55:35 -0800

    init push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
 import logo from './logo.svg';
+import { useState, useCallback } from 'react';
+
 import './App.css';
+import AudioPlayer from 'react-modular-audio-player';
+import bh42 from './audio/bh42.mp3';
+import cherrywine from './audio/cherrywine.mp3'
+import dawg from './audio/dawg.mp3'
+import madness from './audio/madness.mp3'
+import hangover from './audio/hangover.mp3'
+import laid from './audio/laid.mp3'
+import tamiko from './audio/tamiko.mp3'
+
+import cover from './img/tck.png';
+
 
 function App() {
+
+  let audioFiles = [
+    {
+      src: cherrywine,
+      title: "cherry wine",
+    },
+    {
+      src: bh42,
+      title: "BH 42 take 1",
+    },
+    {
+      src: dawg,
+      title: "dawg fuck you",
+    },
+    {
+      src: madness,
+      title: "madness for your eyes",
+    },
+    {
+      src: hangover,
+      title: "hangover buncha ideas",
+    },
+    {
+      src: laid,
+      title: "laid back melody but in jazz",
+    },
+    {
+      src: tamiko,
+      title: "tamiko jones",
+    },
+  ];
+  
+  //inside Component render()
+  
+    
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={cover} className="App-logo" alt="logo" />
+        <AudioPlayer
+        audioFiles={audioFiles}/>
+        <h1>todo: album name</h1>
+        <p>cherry wine</p>
+        <p>bh42</p>
+        <p>dawg fuck you</p>
+        <p>madness for your eyes</p>
+        <p>hangover buncha ideas</p>
+        <p>laid back melody but in jazz</p>
+        <p>tamiko jones</p>
       </header>
+
+    
     </div>
   );
 }
